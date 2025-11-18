421ab91784d23560f05a4574112e6b4e965c6a6b
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-11-18 20:16:38 +0100

    corrección de la ruta por defecto de swagger hacia la api

--- clinica-backend/config/l5-swagger.php
+++ clinica-backend/config/l5-swagger.php
@@ -257,7 +257,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://api.clinicamv.lol'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
         ],
     ],
 ];
