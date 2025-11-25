aae303b9bd75ca766a81cd02ed8d355673ce9cde
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-11-25 20:48:36 +0100

    Se cambia la configuración de swagger para que gestione las rutas en la api de forma relativa ya que las rutas absolutas las gestiona Traefik.

--- clinica-backend/config/l5-swagger.php
+++ clinica-backend/config/l5-swagger.php
@@ -18,7 +18,8 @@ return [
                 /*
                  * Edit to include full URL in ui for assets
                  */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', false),
+                //Ponemos true para que coja la ruta absoluta y no relativa. En este caso necesitamos las rutas relativas, porque las rutas las gestiona el proxy de Traefik
 
                 /*
                  * Edit to set path where swagger ui assets should be stored
